af88e9282d00894d5e20930b49dde42f60145322
ojpro <contact@ojpro.me>
2023-01-21 14:01:17 +0100

    ✅ Completed the Login Screen

---
+++ lib/screens/login.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  // Fields Controllers
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.orangeAccent,
+        leading: const Icon(
+          Icons.arrow_back,
+          color: Colors.white,
+        ),
+        title: const Text(
+          "Login",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              width: double.infinity,
+              child: Column(
+                children: [
+                  Text(
+                    "Welcome Back!",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  TextFormField(
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      labelText: "Email Address",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.email),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: passwordController,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      labelText: "Enter Password",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: Icon(Icons.remove_red_eye),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 20,
+                    ),
+                    elevation: 0,
+                    color: Colors.orangeAccent,
+                    onPressed: () {
+                      print(emailController.text);
+                      print("\n ");
+                      print(passwordController.text);
+                    },
+                    child: const Text(
+                      "Login",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "You don't have an account?",
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Sign-up",
+                          style: TextStyle(color: Colors.orangeAccent),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
