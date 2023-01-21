57fe383e5623be044fbc4000b063efa6d5e093ff
ojpro <contact@ojpro.me>
2023-01-21 22:11:17 +0100

    🏖 Improved the look of the login screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -8,105 +8,87 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.orangeAccent,
-        leading: const Icon(
-          Icons.arrow_back,
-          color: Colors.white,
-        ),
-        title: const Text(
-          "Login",
-          style: TextStyle(
-            color: Colors.white,
-          ),
-          textAlign: TextAlign.center,
-        ),
-      ),
       body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
+        child: Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Welcome Back!",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              width: double.infinity,
-              child: Column(
+              SizedBox(
+                height: 30,
+              ),
+              TextFormField(
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  labelText: "Email Address",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: passwordController,
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: "Enter Password",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: Icon(Icons.remove_red_eye),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              MaterialButton(
+                minWidth: double.infinity,
+                padding: const EdgeInsets.symmetric(
+                  vertical: 20,
+                ),
+                elevation: 0,
+                color: Colors.orangeAccent,
+                onPressed: () {
+                  print(emailController.text);
+                  print("\n ");
+                  print(passwordController.text);
+                },
+                child: const Text(
+                  "Login",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                    "Welcome Back!",
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
+                    "You don't have an account?",
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Sign-up",
+                      style: TextStyle(color: Colors.orangeAccent),
                     ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  TextFormField(
-                    controller: emailController,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                      labelText: "Email Address",
-                      border: OutlineInputBorder(),
-                      prefixIcon: Icon(Icons.email),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    controller: passwordController,
-                    keyboardType: TextInputType.visiblePassword,
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      labelText: "Enter Password",
-                      border: OutlineInputBorder(),
-                      prefixIcon: Icon(Icons.lock),
-                      suffixIcon: Icon(Icons.remove_red_eye),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  MaterialButton(
-                    minWidth: double.infinity,
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 20,
-                    ),
-                    elevation: 0,
-                    color: Colors.orangeAccent,
-                    onPressed: () {
-                      print(emailController.text);
-                      print("\n ");
-                      print(passwordController.text);
-                    },
-                    child: const Text(
-                      "Login",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "You don't have an account?",
-                      ),
-                      TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          "Sign-up",
-                          style: TextStyle(color: Colors.orangeAccent),
-                        ),
-                      ),
-                    ],
                   ),
                 ],
               ),
-            ),
+            ],
           ),
         ),
       ),
