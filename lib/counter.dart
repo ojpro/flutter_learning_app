eb351a17a8d27407910929388e5c700498b0c162
ojpro <contact@ojpro.me>
2023-02-18 00:27:08 +0100

    merging from counter

---
+++ lib/counter.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class CounterScreen extends StatefulWidget {
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Counter+"),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                if (counter > 0) {
+                  setState(() => counter--);
+                }
+              },
+              child: const Text("-"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                "$counter",
+                style:
+                    const TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() => counter++);
+              },
+              child: const Text("+"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
